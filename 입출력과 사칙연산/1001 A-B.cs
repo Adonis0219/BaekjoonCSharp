30a96a3b624ff9464e15743e52d94b24e3b08217
Adonis <wintiger0908@gmail.com>
2026-02-10 10:07:52 +0900

    1001, 1008, 10998 풀이

---
+++ 입출력과 사칙연산/1001 A-B.cs
@@ -0,0 +1,12 @@
+﻿namespace 입출력과_사칙연산
+{
+    internal class AminusB
+    {
+        static void Main()
+        {
+            string[] s = Console.ReadLine().Split();
+
+            Console.WriteLine(int.Parse(s[0]) - int.Parse(s[1]));
+        }
+    }
+}
---
+++ 입출력과 사칙연산/1008 AdivB.cs
@@ -0,0 +1,12 @@
+﻿namespace 입출력과_사칙연산
+{
+    internal class AdivB
+    {
+        static void Main()
+        {
+            string[] s = Console.ReadLine().Split();
+
+            Console.WriteLine(double.Parse(s[0]) / double.Parse(s[1]));
+        }
+    }
+}

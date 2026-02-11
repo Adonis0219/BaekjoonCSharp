b21a3c581382f619ab619ac915c9f2977fe6378c
Adonis <wintiger0908@gmail.com>
2026-02-11 21:50:41 +0900

    10869, 10926 문제풀이

---
+++ 입출력과 사칙연산/10869 사칙연산.cs
@@ -0,0 +1,19 @@
+﻿namespace 입출력과_사칙연산
+{
+    internal class 사칙연산
+    {
+        static void Main()
+        {
+            string[] s = Console.ReadLine().Split();
+
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+        }
+    }
+}
---
+++ 입출력과 사칙연산/10926 QQE.cs
@@ -0,0 +1,12 @@
+﻿namespace 입출력과_사칙연산
+{
+    internal class QQE
+    {
+        static void Main()
+        {
+            string id = Console.ReadLine();
+
+            Console.WriteLine(id + "??!");
+        }
+    }
+}

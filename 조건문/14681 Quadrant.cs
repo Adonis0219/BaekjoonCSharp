22926dea48d777b63464b41394497819b40dd439
Adonis <wintiger0908@gmail.com>
2026-02-15 22:48:02 +0900

    14681 문제 풀이

---
+++ 조건문/14681 Quadrant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 조건문
+{
+    internal class Quadrant
+    {
+        static void Main()
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (x > 0 && y > 0) Console.WriteLine(1);
+            else if (x < 0 && y > 0) Console.WriteLine(2);
+            else if (x < 0 && y < 0) Console.WriteLine(3);
+            else if (x > 0 && y < 0) Console.WriteLine(4);
+        }
+    }
+}

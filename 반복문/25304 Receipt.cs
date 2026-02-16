a288dd11b4f59278aaeae67861eab17498043c04
Adonis <wintiger0908@gmail.com>
2026-02-16 19:43:32 +0900

    25304 25314 문제 풀이

---
+++ 반복문/25304 Receipt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 반복문
+{
+    internal class Receipt
+    {
+        static void Main()
+        {
+            int X = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+
+                X -= int.Parse(s[0]) * int.Parse(s[1]);                
+            }
+
+            Console.WriteLine((X == 0) ? "Yes" : "No");
+        }
+    }
+}
---
+++ 반복문/25314 CodingIsPhysical.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 반복문
+{
+    internal class CodingIsPhysical
+    {
+        static void Main()
+        {
+            string printS = "";
+
+            int N = int.Parse(Console.ReadLine()) / 4;
+
+            for (int i = 0; i < N; i++)
+            {
+                printS += "long ";
+            }
+
+            Console.WriteLine(printS + "int");
+        }
+    }
+}

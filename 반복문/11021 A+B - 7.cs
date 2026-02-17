92a71a59510e6c1bbac189e3a5e30141aa23f618
Adonis <wintiger0908@gmail.com>
2026-02-17 15:40:18 +0900

    11021 문제 풀이

---
+++ 반복문/11021 A+B - 7.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 반복문
+{
+    internal class AplusB_7
+    {
+        static void Main()
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= T; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+
+                int sum = int.Parse(s[0]) + int.Parse(s[1]);
+
+                Console.WriteLine("Case #" + i + ": " + sum);
+            }
+        }
+    }
+}

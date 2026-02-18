61ab82e153660fc7947841e7b6e67219f2595005
Adonis <wintiger0908@gmail.com>
2026-02-18 21:36:52 +0900

    11022, 2438 문제 풀이

---
+++ 반복문/11022 A+B - 8.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 반복문
+{
+    internal class AplusB_8
+    {
+         static void Main()
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= T; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+
+                int a = int.Parse(s[0]), b = int.Parse(s[1]);
+                int sum = a + b;
+
+                Console.WriteLine("Case #" + i + ": " + a + " + " + b + " = " + sum);
+            }
+        }
+    }
+}
---
+++ 반복문/2438 Star - 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 반복문
+{
+    internal class Star_1
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            string s = "";
+
+            for (int i = 0; i < N; i++)
+            {
+                s = "";
+                for (int j = 0; j <= i; j++)
+                {
+                    s += "*";
+                }
+                Console.WriteLine(s);
+            }
+        }
+    }
+}

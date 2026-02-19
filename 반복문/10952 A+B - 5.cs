478a44a9d94397ccc17e7df1dc52080ee1f83db8
Adonis <wintiger0908@gmail.com>
2026-02-19 18:47:27 +0900

    10952 2439 문제 풀이

---
+++ 반복문/10952 A+B - 5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 반복문
+{
+    internal class AplusB_5
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                string[] s = Console.ReadLine().Split();
+
+                int a = int.Parse(s[0]);
+                int b = int.Parse(s[1]);
+
+                if (a == 0 && b == 0) break;
+
+                Console.WriteLine(a + b);
+            }
+        }
+    }
+}
---
+++ 반복문/2439 Star - 2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 반복문
+{
+    internal class Star_2
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                string s = "";
+
+                for (int j = N; j > i; j--)
+                {
+                    s += " ";
+                }
+
+                for (int j = 0; j < i; j++)
+                {
+                    s += "*";
+                }
+
+                Console.WriteLine(s);
+            }
+        }
+    }
+}

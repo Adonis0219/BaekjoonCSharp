09597a262a87fccee864413da1d29dd76c5033fa
Adonis <wintiger0908@gmail.com>
2026-02-20 16:22:29 +0900

    10951 문제 풀이 반복문 파트 종료

---
+++ 반복문/10951 A+B - 4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 반복문
+{
+    internal class AplusB_4
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                var s = Console.ReadLine();
+
+                if (s == null) break;
+
+                var nums = s.Split();
+
+                Console.WriteLine(int.Parse(nums[0]) + int.Parse(nums[1]));
+            }           
+        }
+    }
+}

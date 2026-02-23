c3ce64fd8f87e8ada9827954012211fa00ab65d4
Adonis <wintiger0908@gmail.com>
2026-02-23 12:12:27 +0900

    2562 문제 풀이 중간저장

---
+++ 1차원 배열/2562 Max.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1차원_배열
+{
+    internal class Max
+    {
+        static void Main()
+        {
+            int[] arr = new int[9];
+
+            int max = -1, index = -1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                    index = i + 1;
+                }
+            }
+
+            Console.WriteLine(max + "\n" + index);
+        }
+    }
+}

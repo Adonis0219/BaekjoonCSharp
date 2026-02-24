bf31192a979486e592333870bf387dc83a9e2e82
Adonis <wintiger0908@gmail.com>
2026-02-24 22:42:48 +0900

    3052 문제 풀이

---
+++ 1차원 배열/3052 Remain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1차원_배열
+{
+    internal class Remain
+    {
+        static void Main()
+        {
+            int[] remains = new int[42];
+            int count = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+
+                remains[a % 42]++;
+            }
+
+            for (int i = 0; i < remains.Length; i++)
+            {
+                if (remains[i] != 0) count++;
+            }
+
+            Console.WriteLine(count); 
+        }
+    }
+}

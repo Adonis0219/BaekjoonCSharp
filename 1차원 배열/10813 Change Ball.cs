18c11474cd895edd3d3fd6e34b8f829359d55ae1
Adonis <wintiger0908@gmail.com>
2026-02-24 22:27:33 +0900

    10813, 5597 문제 풀이

---
+++ 1차원 배열/10813 Change Ball.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1차원_배열
+{
+    internal class ChangeBall
+    {
+        static void Main()
+        {
+            string[] s = Console.ReadLine().Split();
+
+            int N = int.Parse(s[0]), M = int.Parse(s[1]);
+            int[] arr = new int[N + 1];
+
+            for (int i = 1; i <= N; i++)
+            {
+                // 인덱스 0 -> 1번 바구니
+                arr[i] = i;
+            }
+
+            for (int a = 0; a < M; a++)
+            {
+                string[] s1 = Console.ReadLine().Split();
+                int i = int.Parse(s1[0]), j = int.Parse(s1[1]);
+
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+
+            string printS = "";
+
+            for (int i = 1; i <= N; i++)
+            {
+                printS += arr[i] + " ";
+            }
+
+            Console.WriteLine(printS);
+        }
+    }
+}
---
+++ 1차원 배열/5597 과제 안 내신 분.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1차원_배열
+{
+    internal class _5597_과제_안_내신_분
+    {
+        static void Main()
+        {
+            int[] n = new int[31];  // 인덱스 0번을 제외한 30명 출석부
+            // C#은 배열 초기화 불필요
+
+            // 28번 반복
+            for (int i = 0; i < 28; i++)
+            {
+                int st = int.Parse(Console.ReadLine());
+
+                n[st] = 1;
+            }
+
+            // 1번부터 30번까지 검사
+            for (int i = 1; i <= 30; i++)
+            {
+                if (n[i] == 0) Console.WriteLine(i);
+            }
+        }
+    }
+}

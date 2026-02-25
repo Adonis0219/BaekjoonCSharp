078c4176751f6e79a38522ff0a7c696fa7770985
Adonis <wintiger0908@gmail.com>
2026-02-25 17:35:51 +0900

    10811 1546 문제 풀이 1차원 배열 마무리

---
+++ 1차원 배열/10811 Reverse Basket.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1차원_배열
+{
+    internal class ReverseBasket
+    {
+        static int[] basket = new int[101];
+
+        static void Main()
+        {
+            for (int i = 0; i < basket.Length; i++)
+            {
+                basket[i] = i;
+            }
+
+            string[] nm = Console.ReadLine().Split();
+            int N = int.Parse(nm[0]), M = int.Parse(nm[1]);
+
+            for (int x = 0; x < M; x++)
+            {
+                string[] ij = Console.ReadLine().Split();
+                int i = int.Parse(ij[0]), j = int.Parse(ij[1]);
+
+                for (int y = 0; y <= (j - i) / 2; y++)
+                {
+                    Swap((i + y), (j - y));
+                }
+            }
+
+            string ps = "";
+
+            for (int x = 1; x <= N; x++) ps += basket[x] + " ";
+
+            Console.WriteLine(ps);
+        }
+
+        static void Swap(int a, int b)
+        {
+            int temp = basket[a];
+            basket[a] = basket[b];
+            basket[b] = temp;
+        }
+    }
+}
---
+++ 1차원 배열/1546 Average.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1차원_배열
+{
+    internal class Average
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            float M = 0;
+
+            string[] s = Console.ReadLine().Split();
+            float[] score = Array.ConvertAll(s, float.Parse);
+
+            for (int i = 0; i < N; i++)
+            {               
+                if (score[i] > M) M = score[i];
+            }
+
+            float total = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                score[i] = score[i] / M * 100;
+                total += score[i];
+            }
+
+            Console.WriteLine(total / N);
+        }
+    }
+}

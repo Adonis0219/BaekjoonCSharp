6bb845557a20ceeaced315b568b83a62ca299999
Adonis <wintiger0908@gmail.com>
2026-02-28 20:25:21 +0900

    문자열 공부

--- 문자열/27866 Char String.cs
+++ 문자열/27866 Char String.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 문자열
+{
+    internal class Char_String
+    {
+        static void Main()
+        {
+            string S = Console.ReadLine();
+            int i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(S[i - 1]);
+        }
+    }
+}

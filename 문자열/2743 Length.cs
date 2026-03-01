1c6be1468d7ad53228c65c5be63f79547f57939a
Adonis <wintiger0908@gmail.com>
2026-03-01 21:15:31 +0900

    2743 문제 풀이

---
+++ 문자열/2743 Length.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 문자열
+{
+    internal class Length
+    {
+        static void Main()
+        {
+            string S = Console.ReadLine();
+
+            Console.WriteLine(S.Length);
+        }
+    }
+}

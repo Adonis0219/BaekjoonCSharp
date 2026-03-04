b99ef6ed8115f8e1a3342a95634abedd713e1e80
Adonis <wintiger0908@gmail.com>
2026-03-04 19:13:51 +0900

    2908, 5622 문제 풀이

---
+++ 문자열/2908 SangSoo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 문자열
+{
+    internal class _2908_SangSoo
+    {
+        static void Main()
+        {
+            // 숫자 2개 입력 받기
+            string[] s = Console.ReadLine().Split();
+
+            // 숫자 뒤집기
+            int A = int.Parse(ReverseWord(s[0]));
+            int B = int.Parse(ReverseWord(s[1]));
+
+            // 숫자 비교 후 출력
+            string ps = (A > B ? A : B) + "";
+
+            Console.WriteLine(ps);
+        }
+
+        static string ReverseWord(string _word)
+        {
+            char[] word = _word.ToCharArray();
+
+            char temp = word[0];
+            word[0] = word[2];
+            word[2] = temp;
+
+            string revWord = new string(word);
+
+            return revWord;
+        }
+    }
+}
---
+++ 문자열/5622 Dail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Text;
+
+namespace 문자열
+{
+    internal class _5622_Dail
+    {
+        static void Main()
+        {
+            string nums = Console.ReadLine();
+            int sum = 0;
+
+            foreach (var num in nums)
+            {
+                switch (num)
+                {
+                    case <= 'C': sum += 3; break;
+                    case <= 'F': sum += 4; break;
+                    case <= 'I': sum += 5; break;
+                    case <= 'L': sum += 6; break;
+                    case <= 'O': sum += 7; break;
+                    case <= 'S': sum += 8; break;
+                    case <= 'V': sum += 9; break;
+                    default:     sum += 10;break;
+                }
+            }
+            
+            Console.WriteLine(sum);
+        }
+    }
+}

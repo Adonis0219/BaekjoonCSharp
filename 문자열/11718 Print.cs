164c8b4dabb4fe7646d2ec65f856241065faa563
Adonis <wintiger0908@gmail.com>
2026-03-05 07:22:43 +0900

    11718 문제 풀이 및 문자열 마무리

---
+++ 문자열/11718 Print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 문자열
+{
+    internal class Print
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                string str = Console.ReadLine();
+
+                Console.WriteLine(str);
+
+                if (string.IsNullOrEmpty(str)) return;
+            }
+        }
+    }
+}

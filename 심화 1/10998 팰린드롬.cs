f02f735f0a7e98772c7e4e22aa062bd1e6a79e53
Adonis <wintiger0908@gmail.com>
2026-03-07 15:57:40 +0900

    10988, 1157 문제 풀이

---
+++ 심화 1/10998 팰린드롬.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 심화_1
+{
+    internal class _10998_팰린드롬
+    {
+        static void Main()
+        {
+            string s = Console.ReadLine();
+            bool isPalin = true;
+
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] == s[s.Length - 1 - i])
+                {
+                    isPalin = true;
+                }
+                else
+                {
+                    isPalin = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine(isPalin ? "1" : "0");
+        }
+    }
+}
---
+++ 심화 1/1157 단어 공부.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 심화_1
+{
+    internal class 단어_공부
+    {
+        static void Main()
+        {
+            string s = Console.ReadLine();
+            int[] alpha = new int[26];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] >= 'a') alpha[s[i] - 'a']++;
+                else alpha[s[i] - 'A']++;
+            }
+
+            int max = -1;
+            char ans = '\0';
+
+            for (int i = 0; i < alpha.Length; i++)
+            {
+                if (alpha[i] > max)
+                {
+                    max = alpha[i];
+                    ans = (char)('A' + i);
+                }
+                else if (alpha[i] == max)
+                {
+                    ans = '?';
+                }
+            }
+
+            Console.WriteLine(ans);
+        }
+    }
+}

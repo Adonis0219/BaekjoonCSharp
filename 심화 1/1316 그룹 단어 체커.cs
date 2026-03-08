af0834b486629a91c8b20a8d358a6489c7f5d456
Adonis <wintiger0908@gmail.com>
2026-03-08 21:53:06 +0900

    2941, 1316 문제 풀이

---
+++ 심화 1/1316 그룹 단어 체커.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 심화_1
+{
+    internal class _1316_그룹_단어_체커
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine()), count = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                string s = Console.ReadLine();
+
+                bool[] visited = new bool[26];
+                bool isGroup = true;
+
+                for (int j = 0; j < s.Length; j++)
+                {
+                    int idx = s[j] - 'a';
+
+                    if (!visited[idx])
+                    {
+                        visited[idx] = true;
+                    }
+                    else if (s[j - 1] != s[j])
+                    {
+                        isGroup = false;
+                        break;
+                    }
+                }
+
+                if (isGroup)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ 심화 1/2941 크로아티아 알파벳.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 심화_1
+{
+    internal class _2941_크로아티아_알파벳
+    {
+
+        static void Main()
+        {
+            string s = Console.ReadLine();
+            int count = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'c')
+                {
+                    if (i + 1 < s.Length && (s[i + 1] == '=' || s[i + 1] == '-'))
+                    {
+                        i++;
+                    }
+                }
+                else if (s[i] == 'd')
+                {
+                    if (i + 2 < s.Length && (s[i + 1] == 'z' && s[i + 2] == '='))
+                    {
+                        i += 2;
+                    }
+                    else if (i + 1 < s.Length && s[i + 1] == '-')
+                    {
+                        i++;
+                    }
+                }
+                else if (s[i] == 'l' || s[i] == 'n')
+                {
+                    if (i + 1 < s.Length && s[i + 1] == 'j')
+                    {
+                        i++;
+                    }
+                }
+                else if (s[i] == 's' || s[i] == 'z')
+                {
+                    if (i + 1 < s.Length && s[i + 1] == '=')
+                    {
+                        i++;
+                    }
+                }
+
+                count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}

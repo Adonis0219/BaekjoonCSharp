4a588b9480aede674b8ffc6db53707d2b4082e0a
Adonis <wintiger0908@gmail.com>
2026-03-16 12:55:23 +0900

    2566, 10798

---
+++ 2차원 배열/10798 세로읽기.cs
@@ -0,0 +1,32 @@
+﻿namespace _2차원_배열
+{
+    internal class _10798_세로읽기
+    {
+        static void Main()
+        {
+            string sentence = "";
+            char[,] chars = new char[5, 15];
+
+            for (int i = 0; i < 5; i++)
+            {
+                sentence = Console.ReadLine();
+
+                for (int j = 0; j < sentence.Length; j++)
+                {
+                    chars[i, j] = sentence[j];
+                }
+            }
+
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (chars[j, i] == '\0')
+                        continue;
+
+                    Console.Write(chars[j, i]);
+                }
+            }
+        }
+    }
+}
---
+++ 2차원 배열/2566 최댓값.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2차원_배열
+{
+    internal class _2566_최댓값
+    {
+        static void Main()
+        {
+            int[,] A = new int[9, 9];
+            int max = int.MinValue, max_i = -1, max_j = -1;
+
+            for (int i = 0; i < 9; i++)
+            {
+                string[] token = Console.ReadLine().Split();
+
+                for (int j = 0; j < 9; j++)
+                {
+                    int temp = int.Parse(token[j]);
+                    A[i, j] = temp;
+
+                    if (temp > max)
+                    {
+                        max = temp;
+                        // 0번부터가 아닌 1번부터
+                        max_i = i + 1;
+                        max_j = j + 1;
+                    }
+                }
+            }
+
+            Console.WriteLine(max);
+            Console.WriteLine(max_i + " " + max_j);
+        }
+    }
+}

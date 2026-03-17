7acdcca6e94e706660c9ad4612524dcd8720ba96
Adonis <wintiger0908@gmail.com>
2026-03-17 14:21:45 +0900

    2563 문제풀이 및 2차원 배열 마무리

---
+++ 2차원 배열/2563 색종이.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2차원_배열
+{
+    internal class _2563_색종이
+    {
+        static void Main()
+        {
+            int[,] a = new int[100, 100];
+            int count = int.Parse(Console.ReadLine()), area = 0;
+
+            while (count-- != 0)
+            {
+                string[] token = Console.ReadLine().Split();
+
+                int x = int.Parse(token[0]), y = int.Parse(token[1]);
+
+                for (int i = x; i < x + 10; i++)
+                    for (int j = y; j < y + 10; j++)
+                        a[i, j]++;
+            }
+
+            for (int i = 0; i < 100; i++)
+                for (int j = 0; j < 100; j++)
+                    if (a[i, j] != 0)
+                        area++;
+
+            Console.WriteLine(area);
+        }
+    }
+}

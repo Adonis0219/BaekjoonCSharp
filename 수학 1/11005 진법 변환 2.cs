053e73c23d19a74c0f9c7d987f0cebacd6553f04
Adonis <wintiger0908@gmail.com>
2026-03-18 21:37:19 +0900

    11005 중간 저장

---
+++ 수학 1/11005 진법 변환 2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 수학_1
+{
+    internal class _11005_진법_변환_2
+    {
+        static void Main()
+        {
+            string[] token = Console.ReadLine().Split();
+            int n = int.Parse(token[0]);
+            int b = int.Parse(token[1]);
+
+
+        }
+    }
+}

49c7a6250c65c00c199495b736169660f48cd3b3
Adonis <wintiger0908@gmail.com>
2026-03-27 23:15:37 +0900

    2903 문제 풀이

---
+++ 수학 1/2903 중앙 이동 알고리즘.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 수학_1
+{
+    internal class _2903_중앙_이동_알고리즘
+    {
+        static void Main()
+        {
+            double m = double.Parse(Console.ReadLine());
+            Console.WriteLine((2 * Math.Pow(2, m - 1) + 1) * (2 * Math.Pow(2, m - 1) + 1));
+        }
+    }
+}

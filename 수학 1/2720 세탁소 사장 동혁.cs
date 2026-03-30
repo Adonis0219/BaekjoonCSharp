ec53eeffcfaba5447927883f4e24399b553f9405
Adonis <wintiger0908@gmail.com>
2026-03-30 21:59:42 +0900

    2720, 2903 다시 풀어보기

--- 수학 1/2720 세탁소 사장 동혁.cs
+++ 수학 1/2720 세탁소 사장 동혁.cs
@@ -12,23 +12,19 @@ namespace 수학_1
 
             for (int i = 0; i < t; i++)
             {
-                // 잔돈
                 int money = int.Parse(Console.ReadLine());
 
-                // 25단위 개수
                 int qu = money / 25;
-                // 25 단위로 나눈 나머지를 구함
                 money %= 25;
-                // 위의 과정을 단위 별로 반복
-                int dime = money / 10;
-                money %= 10;
 
-                int nickel = money / 5;
-                money %= 5;
+                int di = money / 10;
+                money = money % 10;
 
-                int penny = money;
+                int ni = money / 5;
+                money = money % 5;
 
-                Console.WriteLine("{0} {1} {2} {3}", qu, dime, nickel, penny);
+                Console.WriteLine($"{qu} {di} {ni} {money}");
+                // Console.WriteLine("{0} {1} {2} {3}", qu, di, ni, pe);
             }
         }
     }

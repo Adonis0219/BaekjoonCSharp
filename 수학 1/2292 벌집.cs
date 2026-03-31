07cd30ad8616e4d61efb53e7f8af4670f2ce3cc8
Adonis <wintiger0908@gmail.com>
2026-03-31 20:07:11 +0900

    2292 다시 풀기

--- 수학 1/2292 벌집.cs
+++ 수학 1/2292 벌집.cs
@@ -8,17 +8,17 @@ namespace 수학_1
     {
         static void Main()
         {
-            // 문제 조건 범위 고려
-            double n = double.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
 
-            if (n == 1) Console.WriteLine(1);
+            if (N == 1)
+                Console.WriteLine(1);
             else
             {
-                int start = 0, gap = 1, answer = 1;
-
                 while (true)
                 {
-                    if (n >= (6 * start + 2) && n <= (6 * (start + gap) + 1))
+                    int start = 0, gap = 1, answer = 1;
+
+                    if (N >= (6 * start) + 2 && N <= (6 * (start + gap) + 1))
                     {
                         Console.WriteLine(answer + 1);
                         return;
@@ -29,7 +29,6 @@ namespace 수학_1
                     answer++;
                 }
             }
-
         }
     }
 }

1c43d778c786fa904363fc92e2562a7a36b85b4b
Adonis <wintiger0908@gmail.com>
2026-04-03 20:52:28 +0900

    1193 문제 풀이

--- 수학 1/1193 분수찾기.cs
+++ 수학 1/1193 분수찾기.cs
@@ -6,5 +6,23 @@ namespace 수학_1
 {
     internal class _1193_분수찾기
     {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int count = 0, sum = 0, idx = 0;
+
+            while (num > count)
+            {
+                count++;
+                sum += count;
+            }
+
+            idx = num - (sum - count);
+
+            if (count % 2 == 1)
+                Console.WriteLine($"{count - idx + 1}/{idx}");
+            else
+                Console.WriteLine($"{idx}/{count - idx + 1}");
+        }
     }
 }

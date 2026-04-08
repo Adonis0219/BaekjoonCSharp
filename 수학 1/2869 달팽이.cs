f4eb78a33ecaddb6c7c1ff40ba7d930536a0576d
Adonis <wintiger0908@gmail.com>
2026-04-08 16:50:45 +0900

    2869 풀이

--- 수학 1/2869 달팽이.cs
+++ 수학 1/2869 달팽이.cs
@@ -6,5 +6,30 @@ namespace 수학_1
 {
     internal class _2869_달팽이
     {
+        static void Main()
+        {
+            string[] token = Console.ReadLine().Split();
+
+            int A = int.Parse(token[0]);
+            int B = int.Parse(token[1]);
+            int V = int.Parse(token[2]);
+
+            int day = 1;
+
+            while (true)
+            {
+                V -= A;
+                
+                if (V <= 0)
+                {
+                    Console.WriteLine(day);
+                    return;
+                }
+
+                V += B;
+
+                day++;
+            }
+        }
     }
 }

90b89df3647fa018470cc1cd882d545a9f99c889
Adonis <wintiger0908@gmail.com>
2026-04-11 20:47:06 +0900

    프로젝트 세팅

---
+++ 배수, 약수와 소수 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace 배수__약수와_소수_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

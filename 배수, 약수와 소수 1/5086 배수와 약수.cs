990d1d87d4ee59c0663d64ed63888f890ad02843
Adonis <wintiger0908@gmail.com>
2026-04-16 06:24:56 +0900

    5086 문제 풀이

---
+++ 배수, 약수와 소수 1/5086 배수와 약수.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 배수__약수와_소수_1
+{
+    internal class 배수와_약수
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                string[] token = Console.ReadLine().Split();
+                int a = int.Parse(token[0]), b = int.Parse(token[1]);
+
+                if (a == 0 && b == 0) break;
+
+                if (a % b == 0) Console.WriteLine("multiple");
+                else if (b % a == 0) Console.WriteLine("factor");
+                else Console.WriteLine("neither");            
+            }
+        }
+    }
+}
